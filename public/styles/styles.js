df2c72ac6a3c8a2fd3a415becc7e16f6d781c1f9
AdamMenard <adammenard888@gmail.com>
2017-12-19 20:40:01 -0800

    added seed.js file

--- public/styles/styles.js
+++ public/styles/styles.js
@@ -2,14 +2,14 @@
   background-image: url("https://images.pexels.com/photos/716906/pexels-photo-716906.jpeg?w=940&h=650&dpr=2&auto=compress&cs=tinysrgb");
   background-size: cover;
   background-repeat: no-repeat;
-  color: #EEE;
+  color: #AEA;
 }
 .jumbotron .container {
-  height: 350px;
+  height: 400px;
 }
 
 .inline-header {
   display: inline-block;
   vertical-align: baseline;
-  margin-right: 12px;
+  margin-right: 20px;
 }
